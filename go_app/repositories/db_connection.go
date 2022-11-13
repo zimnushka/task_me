0041d7c1228857fbeb194dd792c335fa6ab0ef7c
Kirill_Zimnukhov <g741980@gmail.com>
2022-11-14 00:58:40 +0300

    refactor layers

--- go_app/repositories/db_connection.go
+++ go_app/repositories/db_connection.go
@@ -6,17 +6,9 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func getDB() sql.DB {
-	db, err := sql.Open("mysql", "root:43WYOH5l8W1I@tcp(192.168.17.9:3306)/taskMe")
-	if err != nil {
-		panic(err.Error())
-	}
-	return *db
+type TaskMeDB struct {
 }
-func queryDB(db sql.DB, query string) *sql.Rows {
-	results, err := db.Query(query)
-	if err != nil {
-		panic(err.Error())
-	}
-	return results
+
+func (TaskMeDB) GetDB() (*sql.DB, error) {
+	return sql.Open("mysql", "root:43WYOH5l8W1I@tcp(192.168.17.9:3306)/taskMe")
 }
--- go_app/use_cases/auth.go
+++ go_app/use_cases/auth.go
@@ -1,31 +1,43 @@
 package usecases
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 
 	"github.com/zimnushka/task_me_go/go_app/models"
 	"github.com/zimnushka/task_me_go/go_app/repositories"
 )
 
-type AuthUseCase struct{}
-
-func (useCase *AuthUseCase) Register(user models.User) (string, error) {
-	user.Id = nil
-	repositories.AddUser(user)
-	query := fmt.Sprintf("SELECT * FROM users WHERE email = '%s'", user.Email)
-	user = *repositories.GetUser(query)
-	s, e := json.Marshal(user)
-	return string(s), e
+type AuthUseCase struct {
+	userRepository repositories.UserRepository
 }
-func (useCase *AuthUseCase) Login(email, password string) (string, error) {
-	query := fmt.Sprintf("SELECT * FROM users WHERE email = '%s' AND password = '%s'", email, password)
-	user := repositories.GetUser(query)
-	if user == nil {
-		var err error
-		return "", err
+
+func (useCase *AuthUseCase) Register(user models.User) (*models.User, error) {
+	if user.Email == "" || user.Password == "" {
+		return nil, errors.New("Password or email is empty")
 	}
-	s, e := json.Marshal(user)
-	return string(s), e
+	user.Id = nil
+	userWithEmail, _ := useCase.userRepository.GetUserFromEmail(user.Email)
+	if userWithEmail != nil {
+		return nil, errors.New("User with this email was created")
+	}
+	err := useCase.userRepository.AddUser(user)
+	if err != nil {
+		return nil, err
+	}
+	return useCase.userRepository.GetUserFromEmail(user.Email)
+}
+func (useCase *AuthUseCase) Login(email, password string) (*models.User, error) {
+	if email == "" || password == "" {
+		return nil, errors.New("Password or email is empty")
+	}
+	user, err := useCase.userRepository.GetUserFromEmail(email)
+
+	if err != nil {
+		return nil, err
+	}
+	if user.Password != password {
+		return nil, errors.New("User with this parametrs not found")
+	}
+	return user, nil
 
 }

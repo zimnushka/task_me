7fec6190f0b12ee829a5f1abbd72250ff3647699
Kirill_Zimnukhov <g741980@gmail.com>
2022-11-16 00:36:31 +0300

    add token check

--- go_app/repositories/db_connection.go
+++ go_app/repositories/db_connection.go
@@ -8,14 +8,13 @@ import (
 )
 
 type TaskMeDB struct {
+}
+
+func (TaskMeDB) GetDB() (*sql.DB, error) {
 	const user = "root"
 	const password = "43WYOH5l8W1I"
 	const url = "192.168.17.9:3306"
 	const db = "taskMe"
-}
-
-func (TaskMeDB) GetDB() (*sql.DB, error) {
-	
 
 	return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, url, db))
 }
--- go_app/use_cases/auth.go
+++ go_app/use_cases/auth.go
@@ -1,30 +1,61 @@
 package usecases
 
 import (
+	"encoding/base64"
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/zimnushka/task_me_go/go_app/models"
 	"github.com/zimnushka/task_me_go/go_app/repositories"
 )
 
+const salt = "79dbeb81-6582-419d-a61a-ba5a75a293c7"
+
 type AuthUseCase struct {
 	userRepository repositories.UserRepository
 }
 
-func (useCase *AuthUseCase) Register(user models.User) (*models.User, error) {
+func (useCase *AuthUseCase) CheckToken(token string) (*models.User, error) {
+	data, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return nil, err
+	}
+	words := strings.Split(fmt.Sprintf("%q\n", data), "//")
+	if words[len(words)-1] == salt {
+		id, err := strconv.Atoi(words[0])
+		if err != nil {
+			return nil, err
+		}
+		return useCase.userRepository.GetUserFromId(id)
+	}
+	return nil, errors.New("wrong auth")
+}
+
+func (useCase *AuthUseCase) createToken(user models.User) string {
+
+	parametrs := fmt.Sprintf("%s//%d", salt, user.Id)
+	return base64.StdEncoding.EncodeToString([]byte(parametrs))
+
+}
+
+func (useCase *AuthUseCase) Register(user models.User) (string, error) {
 	if user.Email == "" || user.Password == "" {
-		return nil, errors.New("Password or email is empty")
+		return "", errors.New("Password or email is empty")
 	}
 	user.Id = nil
 	userWithEmail, _ := useCase.userRepository.GetUserFromEmail(user.Email)
 	if userWithEmail != nil {
-		return nil, errors.New("User with this email was created")
+		return "", errors.New("User with this email was created")
 	}
 	err := useCase.userRepository.AddUser(user)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return useCase.userRepository.GetUserFromEmail(user.Email)
+	newUser, err := useCase.userRepository.GetUserFromEmail(user.Email)
+
+	return useCase.createToken(*newUser), nil
 }
 func (useCase *AuthUseCase) Login(email, password string) (*models.User, error) {
 	if email == "" || password == "" {
